3f2d2b9f89b3a1fa226fe4a7815ef0141018bed3
dltjsam <dltjsam@gmail.com>
2022-10-14 13:53:55 +0900

    Day4

--- sql 수업/서브쿼리.sql
+++ sql 수업/서브쿼리.sql
@@ -102,6 +102,20 @@ order by avg_salary
 
 
 -- 3-2) 복수행 연산자 : in, not, in, any, all
+-- any 사용법a
+-- 1. =any : in과 동일
+-- 2. >any, >any : 최소값 
+-- 3. <any, <=any : 최대값
+-- 4. !=any : not in과 동일
+
+-- all 사용법
+-- 1. =all : x
+-- 2. >all, >=all : 최대값 (서브쿼리에 있는 모든 데이터보다 커야함)
+-- 3. <all. <=all : 최소값 (서브쿼리에 있는 모든 데이터보다 작아야함)
+-- 4. !=all : 
+
+
+
 -- 실습문제3 : 현재 급여가 50000이상인 직원의 이름과 급여를 출력하세요.
 -- sol1)
 select a.first_name, b.salary
@@ -112,4 +126,57 @@ select a.first_name, b.salary
 order by b.salary;
 
 -- sol2)
+select a.first_name, b.salary
+	from employees a, salaries b
+    where a.emp_no = b.emp_no
+		and b.to_date like '9999%'
+        and (a.emp_no, b.salary) in (select emp_no, salary -- in은 포함되어있느냐의 개념
+										from salaries
+										where to_date like '9999%'
+											and salary >= 50000)
+order by b.salary;
 
+-- 실습문제4 : 현재, 각 부서별로 최고 월급을 받는 직원의 이름, 부서이름, 월급을 출력하세요.
+-- 출력예시 : 둘리 총무 200, 또치 개발 400
+
+select a.dept_no, max(salary) as max_salary -- 각 부서별 최고월급을 일단 구함
+	from dept_emp a, salaries b
+    where a.emp_no = b.emp_no
+		and a.to_date like '9999%'
+        and b.to_date like '9999%'
+	group by a.dept_no;
+    
+-- sol1) where절 서브쿼리
+select c.first_name, a.dept_name, d.salary
+	from departments a, dept_emp b, employees c, salaries d
+    where a.dept_no = b.dept_no
+		and b.emp_no = c.emp_no
+        and c.emp_no = d.emp_no
+        and b.to_date like '9999%'
+        and d.to_date like '9999%'
+        and (a.dept_no, d.salary) in (select a.dept_no, max(salary) as max_salary
+										from dept_emp a, salaries b
+										where a.emp_no = b.emp_no
+											and a.to_date like '9999%'
+											and b.to_date like '9999%'
+									  group by a.dept_no);
+                                      
+-- sol2) from절 서브쿼리
+select c.first_name, a.dept_name, d.salary
+	from departments a,
+		 dept_emp b,
+         employees c,
+         salaries d,
+         (select a.dept_no, max(salary) as max_salary
+			from dept_emp a, salaries b
+			where a.emp_no = b.emp_no
+				and a.to_date like '9999%'
+				and b.to_date like '9999%'
+			group by a.dept_no) e
+    where a.dept_no = b.dept_no
+		and b.emp_no = c.emp_no
+        and c.emp_no = d.emp_no
+        and e.dept_no = a.dept_no
+        and b.to_date like '9999%'
+        and d.to_date like '9999%'
+        and e.max_salary = d.salary;
