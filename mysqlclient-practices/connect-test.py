7d2f4edbc415db299544ed0cc8f6f426d28b1862
dltjsam <dltjsam@gmail.com>
2022-10-18 10:05:05 +0900

    Day6

--- mysqlclient-practices/connect-test.py
+++ mysqlclient-practices/connect-test.py
@@ -1,4 +1,20 @@
-from MySQLdb import connect
+from MySQLdb import connect, OperationalError
 
-#연결
-connect(user = 'webdb',password = 'webdb',host = 'localhost')
+
+try :
+    #연결
+    db = connect(
+            user = 'webdb',
+            password = 'webdb',
+            host = '127.0.0.1', #'localhost'
+            port = 3306,
+            db = 'webdb',
+            charset = 'utf8') #MySQL에서는 'utf-8'로 쓰면 오류
+    print("success")
+except OperationalError as e:
+    print('error!!!!')
+
+print('end!!!')
+
+#cursor 생성
+#cursor = db.cursor()
