a9311c18824917c47de58753b761731e1aeb1feb
dltjsam <dltjsam@gmail.com>
2022-11-04 20:11:00 +0900

    sql

--- jdbc-practices/sql/bookshop.sql
+++ jdbc-practices/sql/bookshop.sql
@@ -1,3 +1,39 @@
 show tables;
 desc book;
 desc author;
+
+
+-- author insert
+insert into author values(null, '원수연');
+
+
+-- author select
+select * from author;
+
+
+-- author deleteall;
+delete from author;
+
+
+-- book insert
+insert into book values(null, 'test', '재고있음', 3);
+
+-- book select
+select * from book;
+
+-- book delect all;
+delete from book;
+
+-- findAll
+select a.no, a.title, b.name, a.status 
+	from book a, author b
+    where a.author_no = b.no
+order by a.no asc;
+
+-- update status
+update book 
+	set status = '재고있음'
+	where no = 1;
+
+    
+
